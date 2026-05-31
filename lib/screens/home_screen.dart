e93a3a90d578854fbd68f2925ab078619dd92844
Andres Diaz <andresmigueldiazarismendi@gmail.com>
2026-05-31 16:40:11 -0400

    muetra el valor del contador

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,24 +8,29 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>(){
-  int counter = 0; 
-
-  void _increment(){
-    setState( () => counter++);
-  }
-  void _decrement(){
-    setState( () => counter--);
-  }
-  void _reset(){
-    setState( () => counter = 0);
-  }
-}
-
 class _HomeScreenState extends State<HomeScreen> {
   
+  int counter = 0;
+
+  void _increment() {
+    setState(() => counter++);
+  }
+
+  void _decrement() {
+    setState(() => counter--);
+  }
+
+  void _reset() {
+    setState(() => counter = 0);
+  } 
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Contador')),
+      body: Center(
+        child: Text('$counter', style: const TextStyle(fontSize: 80)),
+      ),
+    );
   }
 }
