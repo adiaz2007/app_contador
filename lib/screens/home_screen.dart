b5b6abdf7df9d797677a7dd67b14f6a7d32acab2
Andres Diaz <andresmigueldiazarismendi@gmail.com>
2026-05-31 16:31:09 -0400

    sumar, restar y resetear

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+ 
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>(){
+  int counter = 0; 
+
+  void _increment(){
+    setState( () => counter++);
+  }
+  void _decrement(){
+    setState( () => counter--);
+  }
+  void _reset(){
+    setState( () => counter = 0);
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
